4ee33c8a8a4c6b6f1d0d254e92987130f66fc6cf
aalyoona <aalyoona28@gmail.com>
2023-03-16 15:26:21 +0300

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventManager.RegisterClassHandler(typeof(Button), Button.ClickEvent, new RoutedEventHandler(ButtonMain));
+        }
+
+        private void ButtonMain(object sender, RoutedEventArgs eventArgs)
+        {
+            Button B = (Button)sender;
+
+            switch (B.Content)
+            {
+                case "=":
+                    string value = new DataTable().Compute(Screen.Text, null).ToString();
+                    Screen.Text = value;
+                    break;
+                case "AC":
+                    Screen.Text = "";
+                    break;
+                default:
+                    Screen.Text += B.Content;
+                    break;
+            }
+        }
+    }
+}
